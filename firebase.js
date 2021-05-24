ca5024ae51c0966863836f3a18da1164f86859c8
Amrut-47 <gpkp156027@gmail.com>
2021-05-24 12:47:03 +0530

    auth and redux stuff

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCt1dXyo-Y_NutIkDuAe6t7aS8zxEtRwhc",
+  authDomain: "clone-9ef60.firebaseapp.com",
+  projectId: "clone-9ef60",
+  storageBucket: "clone-9ef60.appspot.com",
+  messagingSenderId: "915493862546",
+  appId: "1:915493862546:web:f2f5b2d3bc86550d277a4b"
+};
